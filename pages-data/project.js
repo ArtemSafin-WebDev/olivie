c04d9ab6966fa7e5bd26385c44b5968eee3b16b7
Artem Safin <artemsafin.webdev@gmail.com>
2025-03-04 12:29:47 +0300

    Фикс сборки

---
+++ pages-data/project.js
@@ -0,0 +1,7 @@
+const project = {
+  "/project.html": {
+    title: "Кейс",
+  },
+};
+
+export default project;
--- pages.config.js
+++ pages.config.js
@@ -1,11 +1,13 @@
 import about from "./pages-data/about";
 import home from "./pages-data/home";
 import portfolio from "./pages-data/portfolio";
+import project from "./pages-data/project";
 
 const pagesConfig = {
   ...home,
   ...portfolio,
   ...about,
+  ...project,
 };
 
 export default pagesConfig;
