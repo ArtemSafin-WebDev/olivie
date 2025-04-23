ad9d4a1424c7ece0e6e864c6dc22ea99d8299ce2
Artem Safin <artemsafin.webdev@gmail.com>
2025-04-23 19:48:40 +0300

    По курсору

--- src/js/cursor.ts
+++ src/js/cursor.ts
@@ -19,43 +19,68 @@ export default function cursor() {
     yTo(y);
   });
 
-  const hoverableItems = Array.from(
-    document.querySelectorAll<HTMLElement>(
-      ".portfolio-card, .reviews__slider-card, .portfolio__slider-card, .formats__card, .ideas__slider-card, .project-gallery__card, .reviews-catalog__card-image-wrapper-inner"
-    )
-  );
+  const initCursorEffects = () => {
+    const abortController = new AbortController();
+    const signal = abortController.signal;
+    const hoverableItems = Array.from(
+      document.querySelectorAll<HTMLElement>(
+        ".portfolio-card, .reviews__slider-card, .portfolio__slider-card, .formats__card, .ideas__slider-card, .project-gallery__card, .reviews-catalog__card-image-wrapper-inner"
+      )
+    );
 
-  hoverableItems.forEach((element) => {
-    element.addEventListener("mouseenter", () => {
-      cursorLayers.forEach((layer) => layer.classList.remove("active"));
-      let currentLayer: HTMLElement | null | undefined = null;
-      if (element.matches(".reviews__slider-card")) {
-        currentLayer = cursorLayers.find(
-          (layer) => layer.dataset.cursor === "orange"
-        );
-      } else if (element.matches(".portfolio-card")) {
-        currentLayer = cursorLayers.find(
-          (layer) => layer.dataset.cursor === "green"
-        );
-      } else if (
-        element.matches(
-          ".formats__card, .project-gallery__card, .reviews-catalog__card-image-wrapper-inner"
-        )
-      ) {
-        currentLayer = cursorLayers.find(
-          (layer) => layer.dataset.cursor === "blue"
-        );
-      } else {
-        currentLayer = cursorLayers.find(
-          (layer) => layer.dataset.cursor === "violet"
-        );
-      }
+    hoverableItems.forEach((element) => {
+      element.addEventListener(
+        "mouseenter",
+        () => {
+          cursorLayers.forEach((layer) => layer.classList.remove("active"));
+          let currentLayer: HTMLElement | null | undefined = null;
+          if (element.matches(".reviews__slider-card")) {
+            currentLayer = cursorLayers.find(
+              (layer) => layer.dataset.cursor === "orange"
+            );
+          } else if (element.matches(".portfolio-card")) {
+            currentLayer = cursorLayers.find(
+              (layer) => layer.dataset.cursor === "green"
+            );
+          } else if (
+            element.matches(
+              ".formats__card, .project-gallery__card, .reviews-catalog__card-image-wrapper-inner"
+            )
+          ) {
+            currentLayer = cursorLayers.find(
+              (layer) => layer.dataset.cursor === "blue"
+            );
+          } else {
+            currentLayer = cursorLayers.find(
+              (layer) => layer.dataset.cursor === "violet"
+            );
+          }
 
-      currentLayer?.classList.add("active");
-      document.body.classList.add("custom-cursor");
+          currentLayer?.classList.add("active");
+          document.body.classList.add("custom-cursor");
+        },
+        {
+          signal,
+        }
+      );
+      element.addEventListener(
+        "mouseleave",
+        () => {
+          document.body.classList.remove("custom-cursor");
+        },
+        {
+          signal,
+        }
+      );
     });
-    element.addEventListener("mouseleave", () => {
-      document.body.classList.remove("custom-cursor");
-    });
+
+    return () => abortController.abort();
+  };
+
+  let removeCursorEffects = initCursorEffects();
+
+  document.addEventListener("cursor:reinit", () => {
+    removeCursorEffects();
+    removeCursorEffects = initCursorEffects();
   });
 }
